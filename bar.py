76eea4dbfaaf28126fb15b90029ed60cd7a5f3cd
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2026-05-31 09:52:55 +0100

    Add files via upload

--- bar.py
+++ bar.py
@@ -7,27 +7,31 @@ import tkinter as tk
 
 
 class myapps:
-    def __init__(self,root:tk.Tk):
+    def __init__(self,root:tk.Tk,titles:str,w:int,h:int,backgrounds:str,steps:int,colors:str):
         self.root=root
-        self.root.title("bars")
-        self.root.geometry("640x480")
-        self.root.configure(background="black")
-        self.canvas = tk.Canvas(background="black",width=640,height=480)
+        self.root.title(titles)
+        self.root.geometry(str(w)+"x"+str(h))
+        self.root.configure(background=backgrounds)
+        self.canvas = tk.Canvas(background=backgrounds,width=w,height=h)
         self.canvas.pack(padx=0,pady=0)
         c=self.canvas
-        for a in range(0,640,80):
+        for a in range(0,w,steps*4):
             
             
-            c.create_rectangle(a,0,a+20,480,fill="white")
-        for a in range(0,480,80):
+            c.create_rectangle(a,0,a+steps,h,fill=colors)
+        for a in range(0,h,steps*4):
             
             
-            c.create_rectangle(0,a,640,a+20,fill="white")
+            c.create_rectangle(0,a,w,a+steps,fill=colors)
 
 
 
 
+def starts(titles:str,w:int,h:int,backgrounds:str,steps:int,colors:str):
+    root=tk.Tk()
+    apps=myapps(root,titles,w,h,backgrounds,steps,colors)
+    root.mainloop()
 
-root=tk.Tk()
-apps=myapps(root)
-root.mainloop()
+
+
+starts("rets",640,480,"black",20,"white")
